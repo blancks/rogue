78062f041b827b12fe21222db2f4f0dfd352fdaf
blancks <s.rotondo90@gmail.com>
2025-06-21 16:00:19 +0200

    Refactor MiddlewareDispatcherTest to account for recent changes

--- test/Unit/Mantle/Routing/Handlers/MiddlewareDispatcherTest.php
+++ test/Unit/Mantle/Routing/Handlers/MiddlewareDispatcherTest.php
@@ -10,16 +10,23 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Rogue\Mantle\Contracts\ContainerInterface;
 use ValueError;
 
 class MiddlewareDispatcherTest extends TestCase
 {
-    public function testHandleWithNoMiddlewareReturnsFinalHandlerResponse()
+    public function testImplementsInterfaces(): void
+    {
+        $dispatcher = new MiddlewareDispatcher();
+        $this->assertInstanceOf(\Rogue\Mantle\Contracts\MiddlewareDispatcherInterface::class, $dispatcher);
+        $this->assertInstanceOf(\Rogue\Mantle\Contracts\ContainerAwareInterface::class, $dispatcher);
+    }
+
+    public function testHandleWithNoMiddlewareReturnsFinalHandlerResponse(): void
     {
         // Arrange
         $request = $this->createMock(ServerRequestInterface::class);
         $response = $this->createMock(ResponseInterface::class);
-
         $finalHandler = $this->createMock(RequestHandlerInterface::class);
         $finalHandler
             ->expects($this->once())
@@ -28,47 +35,86 @@ class MiddlewareDispatcherTest extends TestCase
             ->willReturn($response);
 
         // Act
-        $dispatcher = new MiddlewareDispatcher([], $finalHandler);
+        $dispatcher = new MiddlewareDispatcher();
+        $dispatcher->setMiddlewareStack([]);
+        $dispatcher->setFinalHandler($finalHandler);
         $result = $dispatcher->handle($request);
 
         // Assert
         $this->assertSame($response, $result);
     }
 
-    public function testHandleWithMiddlewareProcessesStack()
+    public function testHandleWithMiddlewareProcessesStack(): void
     {
         // Arrange
         $request = $this->createMock(ServerRequestInterface::class);
         $response = $this->createMock(ResponseInterface::class);
-
         $finalHandler = $this->createMock(RequestHandlerInterface::class);
-        $finalHandler
-            ->expects($this->never())
-            ->method('handle');
+        $finalHandler->expects($this->never())->method('handle');
 
+        $middlewareClass = 'TestMiddleware';
         $middleware = $this->createMock(MiddlewareInterface::class);
-        $middleware
-            ->expects($this->once())
+        $middleware->expects($this->once())
             ->method('process')
             ->with($request, $this->isInstanceOf(MiddlewareDispatcher::class))
             ->willReturn($response);
 
+        $container = $this->getMockContainerInstance($middlewareClass, $middleware);
+
         // Act
-        $dispatcher = new MiddlewareDispatcher([$middleware], $finalHandler);
+        $dispatcher = new MiddlewareDispatcher();
+        $dispatcher->setContainer($container);
+        $dispatcher->setMiddlewareStack([$middlewareClass]);
+        $dispatcher->setFinalHandler($finalHandler);
         $result = $dispatcher->handle($request);
 
         // Assert
         $this->assertSame($response, $result);
     }
 
-    public function testConstructorThrowsOnInvalidMiddleware()
+    public function testConstructorThrowsOnInvalidMiddleware(): void
     {
         // Arrange
         $finalHandler = $this->createMock(RequestHandlerInterface::class);
+        $invalidClass = 'InvalidMiddleware';
         $invalidMiddleware = new class () {};
+        $container = $this->getMockContainerInstance($invalidClass, $invalidMiddleware);
+
+        $dispatcher = new MiddlewareDispatcher();
+        $dispatcher->setContainer($container);
+        $dispatcher->setFinalHandler($finalHandler);
 
         // Act + Assert
         $this->expectException(ValueError::class);
-        new MiddlewareDispatcher([$invalidMiddleware], $finalHandler);
+        $dispatcher->setMiddlewareStack([$invalidClass]);
+    }
+
+    private function getMockContainerInstance(
+        string $middlewareClass,
+        object $middleware
+    ): ContainerInterface {
+        return new class ($middlewareClass, $middleware) implements ContainerInterface {
+            private $class;
+            private $instance;
+            public function __construct($class, $instance)
+            {
+                $this->class = $class;
+                $this->instance = $instance;
+            }
+            public function make(string|object $abstract): object
+            {
+                if ($abstract === $this->class) {
+                    return $this->instance;
+                }
+                throw new \RuntimeException('Unknown class');
+            }
+            public function bind(string $abstract, string $concrete): void
+            {
+            }
+            public function call(array|\Closure $callable, array $parameters = []): mixed
+            {
+                return null;
+            }
+        };
     }
 }
