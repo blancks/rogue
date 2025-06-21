69c570e909bf64ede3cc9f7d111610aa16a6373c
blancks <s.rotondo90@gmail.com>
2025-06-21 15:36:51 +0200

    Add MiddlewareDispatcherInterface for middleware management

---
+++ mantle/Contracts/MiddlewareDispatcherInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rogue\Mantle\Contracts;
+
+use Psr\Http\Server\RequestHandlerInterface;
+
+interface MiddlewareDispatcherInterface extends RequestHandlerInterface
+{
+    /**
+     * Set the middleware stack
+     * @param string[] $middleware List of middleware classnames extending the PSR-15 MiddlewareInterface
+     */
+    public function setMiddlewareStack(array $middleware): void;
+
+    /**
+     * Set the final request handler
+     * @param RequestHandlerInterface $finalHandler
+     */
+    public function setFinalHandler(RequestHandlerInterface $finalHandler): void;
+}
