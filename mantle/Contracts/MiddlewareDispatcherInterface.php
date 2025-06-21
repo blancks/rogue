eebfe40a331e6ef245063944f54b6c07358f1d78
blancks <s.rotondo90@gmail.com>
2025-06-21 16:08:47 +0200

    Fix namespace for ValueError exception in setMiddlewareStack documentation

--- mantle/Contracts/MiddlewareDispatcherInterface.php
+++ mantle/Contracts/MiddlewareDispatcherInterface.php
@@ -11,7 +11,7 @@ interface MiddlewareDispatcherInterface extends RequestHandlerInterface
     /**
      * Set the middleware stack
      * @param string[] $middleware List of middleware classnames extending the PSR-15 MiddlewareInterface
-     * @throws ValueError If an invalid middleware is detected.
+     * @throws \ValueError If an invalid middleware is detected.
      */
     public function setMiddlewareStack(array $middleware): void;
 
